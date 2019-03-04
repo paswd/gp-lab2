59e4cce593b5c061cff4a1c17f6730a0ba311f0d
paswd <paswd@yandex.ru>
2019-03-04 21:35:21 +0300

    Fixed paralleling

--- gp-lab2.cu
+++ gp-lab2.cu
@@ -6,6 +6,21 @@
 
 using namespace std;
 
+/*
+=========
+CONSTANTS
+=========
+*/
+
+const uint32_t COUNTING_SORT_BASE = 256;
+const uint32_t BLOCK_DIM = 1024;
+
+/*
+==========
+STRUCTURES
+==========
+*/
+
 struct Pixel {
 	uint8_t Red;
 	uint8_t Green;
@@ -18,6 +33,12 @@ struct Position {
 	int32_t Y;
 };
 
+/*
+======
+DEVICE
+======
+*/
+
 __device__ double GetIntensity(Pixel pixel) {
 	return (.3 * (double) pixel.Red) + (.59 * (double) pixel.Green) + (.11 * (double) pixel.Blue);
 }
@@ -29,9 +50,26 @@ __device__ bool IsCorrectPos(Position pos, uint32_t height, uint32_t width) {
 	return false;
 }
 
+__device__ void CountingSort(uint8_t *array, uint32_t size) {
+	uint32_t count_array[COUNTING_SORT_BASE];
+	for (uint32_t i = 0; i < COUNTING_SORT_BASE; i++) {
+		count_array[i] = 0;
+	}
+	for (uint32_t i = 0; i < size; i++) {
+		count_array[array[i]]++;
+	}
+	uint32_t current = 0;
+	for (uint32_t i = 0; i < COUNTING_SORT_BASE; i++) {
+		for (uint32_t j = 0; j < count_array[i]; j++) {
+			array[current] = i;
+			current++;
+		}
+	}
+}
+
 __device__ void GetNewPixel(Position pos, uint32_t radius, uint32_t height, uint32_t width,
 		Pixel **map_in, Pixel **map_out) {
-	Position start, end;
+	/*Position start, end;
 	start.X = pos.X - (int32_t) radius;
 	start.Y = pos.Y - (int32_t) radius;
 	end.X = pos.X + (int32_t) radius;
@@ -59,18 +97,29 @@ __device__ void GetNewPixel(Position pos, uint32_t radius, uint32_t height, uint
 			curr++;
 		}
 	}
-	sort(kernel_array_red, kernel_array_red + curr);
-	sort(kernel_array_green, kernel_array_green + curr);
-	sort(kernel_array_blue, kernel_array_blue + curr);
+	CountingSort(kernel_array_red, curr);
+	CountingSort(kernel_array_green, curr);
+	CountingSort(kernel_array_blue, curr);*/
 
-	map_out[pos.X][pos.Y].Red = kernel_array_red[curr / 2];
+
+	/*map_out[pos.X][pos.Y].Red = kernel_array_red[curr / 2];
 	map_out[pos.X][pos.Y].Green = kernel_array_green[curr / 2];
-	map_out[pos.X][pos.Y].Blue = kernel_array_blue[curr / 2];
+	map_out[pos.X][pos.Y].Blue = kernel_array_blue[curr / 2];*/
+	map_out[pos.X][pos.Y].Alpha = map_in[pos.X][pos.Y].Alpha;
+	map_out[pos.X][pos.Y].Red = map_in[pos.X][pos.Y].Red;
+	map_out[pos.X][pos.Y].Green = map_in[pos.X][pos.Y].Green;
+	map_out[pos.X][pos.Y].Blue = map_in[pos.X][pos.Y].Blue;
 
-	delete [] kernel_array_red;
+	/*delete [] kernel_array_red;
 	delete [] kernel_array_green;
-	delete [] kernel_array_blue;
+	delete [] kernel_array_blue;*/
 }
+
+/*
+======
+GLOBAL
+======
+*/
 
 __global__ void MedianFilter(uint32_t radius, uint32_t height, uint32_t width,
 		Pixel **map_in, Pixel **map_out) {
@@ -88,6 +137,12 @@ __global__ void MedianFilter(uint32_t radius, uint32_t height, uint32_t width,
 		}
 	}
 }
+
+/*
+====
+HOST
+====
+*/
 
 __host__ Pixel SetPixel(uint8_t red, uint8_t green, uint8_t blue, uint8_t alpha) {
 	Pixel pixel;
@@ -152,6 +207,7 @@ __host__ void WriteImageToFile(Pixel **pixel, uint32_t height, uint32_t width, s
 			WritePixelToFile(&(pixel)[i][j], file);
 		}
 	}
+	fclose(file);
 }
 
 __host__ void FileGenerator() {
@@ -160,7 +216,7 @@ __host__ void FileGenerator() {
 	uint32_t width = 3;
 	InitPixelMap(&pixel, height, width);
 
-	string filename = "in1.data";
+	string filename = "in.data";
 	pixel[0][0] = SetPixel(1, 2, 3, 0);
 	pixel[0][1] = SetPixel(4, 5, 6, 0);
 	pixel[0][2] = SetPixel(7, 8, 9, 0);
@@ -178,11 +234,48 @@ __host__ void FileGenerator() {
 }
 
 __host__ int main(void) {
-	/*string file_in, file_out;
+	string file_in, file_out;
 	uint32_t radius;
 
-	cin >> file_in >> file_out >> radius;*/
-	FileGenerator();
+	cin >> file_in >> file_out >> radius;
+	//FileGenerator();
+	Pixel **pixel_in;
+	Pixel **pixel_out;
+	uint32_t height, width;
+	ReadImageFromFile(&pixel_in, &height, &width, file_in);
+	//WriteImageToFile(pixel, height, width, "out.data");
+
+	InitPixelMap(&pixel_out, height, width);
+
+	Pixel **cuda_pixel_in;
+	Pixel **cuda_pixel_out;
+
+	size_t pitch;
+	cudaMallocPitch((void**) &cuda_pixel_in, &pitch, width * sizeof(Pixel), height);
+	cudaMallocPitch((void**) &cuda_pixel_out, &pitch, width * sizeof(Pixel), height);
+	cudaMemcpy(cuda_pixel_in, pixel_in, sizeof(Pixel) * width * height, cudaMemcpyHostToDevice);
+
+	dim3 grid_size = dim3((height / BLOCK_DIM) + 1, (width / BLOCK_DIM) + 1, 1);
+    dim3 block_size = dim3(BLOCK_DIM, BLOCK_DIM, 1);
+    cout << grid_size.x << " " << grid_size.y << " " << grid_size.z << endl;
+    cout << block_size.x << " " << block_size.y << " " << block_size.z << endl;
+	MedianFilter<<<grid_size, block_size>>>(radius, height, width, cuda_pixel_in, cuda_pixel_out);
+
+	cudaEvent_t syncEvent;
+
+	cudaEventCreate(&syncEvent);
+	cudaEventRecord(syncEvent, 0);
+	cudaEventSynchronize(syncEvent);
+
+	cudaMemcpy(pixel_out, cuda_pixel_in, sizeof(Pixel) * width * height, cudaMemcpyDeviceToHost);
+
+	cudaFree(cuda_pixel_in);
+	cudaFree(cuda_pixel_out);
+
+	WriteImageToFile(pixel_out, height, width, file_out.c_str());
+
+	DestroyPixelMap(&pixel_in, height);
+	DestroyPixelMap(&pixel_out, height);
 
 	return 0;
 }
