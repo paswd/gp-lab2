fe28e73f7f9644ba6600c2284692d8e379624238
paswd <paswd@yandex.ru>
2019-03-16 11:21:35 +0300

    Counting sort without saving intermediate data

--- gp-lab2.cu
+++ gp-lab2.cu
@@ -3,6 +3,7 @@
 #include <string>
 #include <cstdio>
 #include <cstdlib>
+#include "../lib/cuPrintf.cu"
 
 using namespace std;
 
@@ -13,7 +14,7 @@ CONSTANTS
 */
 
 const uint32_t COUNTING_SORT_BASE = 256;
-const uint32_t BLOCK_DIM = 1024;
+const uint32_t BLOCK_DIM = 32;
 
 /*
 ==========
@@ -50,7 +51,7 @@ __device__ bool IsCorrectPos(Position pos, uint32_t height, uint32_t width) {
 	return false;
 }
 
-__device__ void CountingSort(uint8_t *array, uint32_t size) {
+/*__device__ void CountingSort(uint8_t *array, uint32_t size) {
 	uint32_t count_array[COUNTING_SORT_BASE];
 	for (uint32_t i = 0; i < COUNTING_SORT_BASE; i++) {
 		count_array[i] = 0;
@@ -65,67 +66,106 @@ __device__ void CountingSort(uint8_t *array, uint32_t size) {
 			current++;
 		}
 	}
-}
+}*/
 
 __device__ int32_t GetLinearizedPosition(Position pos, uint32_t height, uint32_t width) {
-	if (pos.X < 0 || pos.Y < 0) {
+	if (!IsCorrectPos(pos, height, width)) {
 		return -1;
+	}
+	return pos.Y * width + pos.X;
+}
+
+__device__ Pixel GetMedianValue(Pixel *map_in, uint32_t height, uint32_t width, Position start, Position end) {
+	uint32_t count_array_red[COUNTING_SORT_BASE];
+	uint32_t count_array_green[COUNTING_SORT_BASE];
+	uint32_t count_array_blue[COUNTING_SORT_BASE];
+	for (uint32_t i = 0; i < COUNTING_SORT_BASE; i++) {
+		count_array_red[i] = 0;
+		count_array_green[i] = 0;
+		count_array_blue[i] = 0;
 	}
 
-	int32_t res = pos.Y * width + pos.X;
-	if ((uint32_t) res >= height * width) {
-		return -1;
+	Position curr_pos;
+
+	uint32_t size = 0;
+
+	for (curr_pos.X = start.X; curr_pos.X <= end.X; curr_pos.X++) {
+		for (curr_pos.Y = start.Y; curr_pos.Y <= end.Y; curr_pos.Y++) {
+			if (!IsCorrectPos(curr_pos, height, width)) {
+				continue;
+			}
+			count_array_red[map_in[GetLinearizedPosition(curr_pos, height, width)].Red]++;
+			count_array_green[map_in[GetLinearizedPosition(curr_pos, height, width)].Green]++;
+			count_array_blue[map_in[GetLinearizedPosition(curr_pos, height, width)].Blue]++;
+
+			size++;
+		}
 	}
+
+	Pixel res;
+	uint32_t tmp_cnt = 0;
+	bool is_break = false;
+	for (uint32_t i = 0; i < COUNTING_SORT_BASE; i++) {
+		for (uint32_t j = 0; j < count_array_red[i]; j++) {
+			if (tmp_cnt == size / 2) {
+				res.Red = i;
+				is_break = true;
+				break;
+			}
+			tmp_cnt++;
+		}
+		if (is_break) {
+			break;
+		}
+	}
+	tmp_cnt = 0;
+	is_break = false;
+	for (uint32_t i = 0; i < COUNTING_SORT_BASE; i++) {
+		for (uint32_t j = 0; j < count_array_green[i]; j++) {
+			if (tmp_cnt == size / 2) {
+				res.Green = i;
+				is_break = true;
+				break;
+			}
+			tmp_cnt++;
+		}
+		if (is_break) {
+			break;
+		}
+	}
+	tmp_cnt = 0;
+	is_break = false;
+	for (uint32_t i = 0; i < COUNTING_SORT_BASE; i++) {
+		for (uint32_t j = 0; j < count_array_blue[i]; j++) {
+			if (tmp_cnt == size / 2) {
+				res.Blue = i;
+				is_break = true;
+				break;
+			}
+			tmp_cnt++;
+		}
+		if (is_break) {
+			break;
+		}
+	}
+	res.Alpha = 0;
 	return res;
 }
 
 __device__ void GetNewPixel(Position pos, uint32_t radius, uint32_t height, uint32_t width,
 		Pixel *map_in, Pixel *map_out) {
-	/*Position start, end;
+	Position start, end;
 	start.X = pos.X - (int32_t) radius;
 	start.Y = pos.Y - (int32_t) radius;
 	end.X = pos.X + (int32_t) radius;
 	end.Y = pos.Y + (int32_t) radius;
 
-	uint32_t kernel_width = (radius * 2 + 1);
-	uint32_t kernel_array_size = kernel_width * kernel_width;
 
-	uint8_t *kernel_array_red = new uint8_t[kernel_array_size];
-	uint8_t *kernel_array_green = new uint8_t[kernel_array_size];
-	uint8_t *kernel_array_blue = new uint8_t[kernel_array_size];
-
-	uint32_t curr = 0;
-	Position curr_pos;
-
-	for (curr_pos.X = start.X; curr_pos.X < end.X; curr_pos.X++) {
-		for (curr_pos.Y = start.Y; curr_pos.Y < end.Y; curr_pos.Y++) {
-			if (!IsCorrectPos(curr_pos, height, width)) {
-				continue;
-			}
-			//kernel_array[curr] = map_in[curr_pos.X][curr_pos.Y];
-			kernel_array_red[curr] = map_in[curr_pos.X][curr_pos.Y].Red;
-			kernel_array_green[curr] = map_in[curr_pos.X][curr_pos.Y].Green;
-			kernel_array_blue[curr] = map_in[curr_pos.X][curr_pos.Y].Blue;
-			curr++;
-		}
-	}
-	CountingSort(kernel_array_red, curr);
-	CountingSort(kernel_array_green, curr);
-	CountingSort(kernel_array_blue, curr);*/
-
-
-	/*map_out[pos.X][pos.Y].Red = kernel_array_red[curr / 2];
-	map_out[pos.X][pos.Y].Green = kernel_array_green[curr / 2];
-	map_out[pos.X][pos.Y].Blue = kernel_array_blue[curr / 2];*/
 	int32_t pos_linear = GetLinearizedPosition(pos, height, width);
-	map_out[pos_linear].Alpha = map_in[pos_linear].Alpha;
-	map_out[pos_linear].Red = map_in[pos_linear].Red;
+	/*map_out[pos_linear].Red = map_in[pos_linear].Red;
 	map_out[pos_linear].Green = map_in[pos_linear].Green;
-	map_out[pos_linear].Blue = map_in[pos_linear].Blue;
-
-	/*delete [] kernel_array_red;
-	delete [] kernel_array_green;
-	delete [] kernel_array_blue;*/
+	map_out[pos_linear].Blue = map_in[pos_linear].Blue;*/
+	map_out[pos_linear] = GetMedianValue(map_in, height, width, start, end);
 }
 
 /*
@@ -136,19 +176,28 @@ GLOBAL
 
 __global__ void MedianFilter(uint32_t radius, uint32_t height, uint32_t width,
 		Pixel *map_in, Pixel *map_out) {
-	Position begin, offset;
+	/*Position begin, offset;
 	begin.X = (int32_t) (blockDim.x * blockIdx.x + threadIdx.x);
 	offset.X = (int32_t) (gridDim.x * blockDim.x);
 	begin.Y = (int32_t) (blockDim.y * blockIdx.y + threadIdx.y);
-	offset.Y = (int32_t) (gridDim.y * blockDim.y);
+	offset.Y = (int32_t) (gridDim.y * blockDim.y);*/
 
 	Position pos;
+	pos.X = blockIdx.x * blockDim.x + threadIdx.x;
+	pos.Y = blockIdx.y * blockDim.y + threadIdx.y;
+	//cuPrintf("TEST2\n");
 
-	for (pos.X = begin.X; pos.X < height; pos.X += offset.X) {
+	/*for (pos.X = begin.X; pos.X < height; pos.X += offset.X) {
 		for (pos.Y = begin.Y; pos.Y < width; pos.Y += offset.Y) {
 			GetNewPixel(pos, radius, height, width, map_in, map_out);
 		}
+	}*/
+
+	//cuPrintf("\n%d:%d\n%d:%d\n======\n", pos.X, pos.Y, height, width);
+	if (pos.X < width && pos.Y < height) {
+		GetNewPixel(pos, radius, height, width, map_in, map_out);
 	}
+	//cuPrintf("Hello world from CUDA\n");
 }
 
 /*
@@ -251,8 +300,28 @@ __host__ void FileGenerator() {
 	WriteImageToFile(pixel, height, width, filename);
 	DestroyPixelMap(&pixel);
 }
+__host__ void FileGeneratorBig(uint32_t height, uint32_t width, string filename) {
+	Pixel *pixel;
+	InitPixelMap(&pixel, height, width);
+
+	for (uint32_t i = 0; i < height; i++) {
+		for (uint32_t j = 0; j < width; j++) {
+			uint8_t curr;
+			if (i == 0 || j == 0 || i == height - 1 || j == width - 1) {
+				curr = 1;
+			} else {
+				curr = 3;
+			}
+			pixel[i * width + j] = SetPixel(curr, curr, curr, 0);
+		}
+	}
+
+	WriteImageToFile(pixel, height, width, filename);
+	DestroyPixelMap(&pixel);
+}
 
 __host__ int main(void) {
+	FileGeneratorBig(128, 128, "inbig.data");
 	string file_in, file_out;
 	uint32_t radius;
 
@@ -282,7 +351,7 @@ __host__ int main(void) {
 	dim3 threads_per_block(width, height);
 	dim3 blocks_per_grid(1, 1);
 
-	if (height * width > BLOCK_DIM){
+	if (height * width > BLOCK_DIM * BLOCK_DIM){
 		threads_per_block.x = BLOCK_DIM;
 		threads_per_block.y = BLOCK_DIM;
 		blocks_per_grid.x = ceil((double) (width) / (double)(threads_per_block.x));
@@ -291,8 +360,17 @@ __host__ int main(void) {
 
 	cout << threads_per_block.x << " " << threads_per_block.y << endl;
 	cout << blocks_per_grid.x << " " << blocks_per_grid.y << endl;
-	
+
+	//cudaPrintfInit();
 	MedianFilter<<<blocks_per_grid, threads_per_block>>>(radius, height, width, cuda_pixel_in, cuda_pixel_out);
+
+	cout << cudaGetErrorString(cudaGetLastError()) << endl;
+
+
+	//cudaPrintfDisplay(stdout, true);
+    //cudaPrintfEnd();
+
+
 
 	cudaEvent_t syncEvent;
 
@@ -312,68 +390,5 @@ __host__ int main(void) {
 	DestroyPixelMap(&pixel_in);
 	DestroyPixelMap(&pixel_out);
 
-	//FileGenerator();
-
 	return 0;
 }
-
-/*__host__ int mainOld(void) {
-	size_t size;
-	cin >> size;
-
-	double *first = new double[size];
-	double *second = new double[size];
-	double *res = new double[size];
-
-	for (size_t i = 0; i < size; i++) {
-		cin >> first[i];
-		//first[i] = i;
-	}
-	for (size_t i = 0; i < size; i++) {
-		cin >> second[i];
-		//second[i] = i;
-	}
-
-	double *cudaFirst;
-	double *cudaSecond;
-	double *cudaRes;
-
-	cudaMalloc((void**) &cudaFirst, sizeof(double) * size);
-	cudaMalloc((void**) &cudaSecond, sizeof(double) * size);
-	cudaMalloc((void**) &cudaRes, sizeof(double) * size);
-
-	cudaMemcpy(cudaFirst, first, sizeof(double) * size, cudaMemcpyHostToDevice);
-	cudaMemcpy(cudaSecond, second, sizeof(double) * size, cudaMemcpyHostToDevice);
-
-	VectorsPairMaximums<<<256, 256>>>(size, cudaFirst, cudaSecond, cudaRes);
-
-	cudaEvent_t syncEvent;
-
-	cudaEventCreate(&syncEvent);
-	cudaEventRecord(syncEvent, 0);
-	cudaEventSynchronize(syncEvent);
-
-	cudaMemcpy(res, cudaRes, sizeof(double) * size, cudaMemcpyDeviceToHost);
-
-	//double *testArr = new double[size];
-	//cudaMemcpy(testArr, cudaFirst, sizeof(double) * size, cudaMemcpyDeviceToHost);
-
-	cudaEventDestroy(syncEvent);
-	cudaFree(cudaFirst);
-	cudaFree(cudaSecond);
-	cudaFree(cudaRes);
-
-	for (size_t i = 0; i < size; i++) {
-		if (i > 0) {
-			cout << " ";
-		}
-		cout << scientific << res[i];
-	}
-	cout << endl;
-
-	delete [] first;
-	delete [] second;
-	delete [] res;
-
-	return 0;
-}*/
